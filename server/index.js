0a43748a3196280fc5929b0238ed1176ceebc4e3
Xavatar3 <153331537+Xavatar3@users.noreply.github.com>
2026-01-19 16:55:02 +0300

    Set up logger.js

--- server/index.js
+++ server/index.js
@@ -1,6 +1,9 @@
 import express from "express";
+import fs from "fs";
 import path from "path";
 import { fileURLToPath } from "url";
+import { logFallback } from "./logger.js"
+//import objectsRouter from "./routes/objects.js";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -8,6 +11,18 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 // Serve frontend
 app.use(express.static(path.join(__dirname, "../src")));
+
+// Middleware
+//app.use(express.json()); // parse JSON
+
+// API routes
+//app.use("/api/objects", objectsRouter);
+
+// Fallback
+app.get(/.*/, (req, res) => {
+  logFallback(req);
+  res.sendFile(path.join(__dirname, "../src/index.html"));
+});
 
 //Listen for requests
 app.listen(PORT, () => console.log(`Server running at http://localhost:${PORT}`));
